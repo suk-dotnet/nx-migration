c41e5247efedf215997466ed10a43df3b3652e58
Sukanya Sengupta <sukanyassg0410@gmail.com>
2024-05-14 21:17:15 +0530

    nx migration commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,28 @@
-function defaultTask(cb) {
-    // place code for your default task here
-    cb();
-  }
+
+// var gulp = require('gulp');
+// function defaultTask(cb) {
+//     // place code for your default task here
+
+//     gulp.task('default', function() {
+//         // gulp.src('./client/index.html')
+//         gulp.src('./src/index.html')
+//             .pipe(gulp.dest('./dist/index.html'))
+//     });
+//     cb();
+//   }
   
-  exports.default = defaultTask
+//   exports.default = defaultTask
+  
+var gulp = require('gulp');
+var del = require('del');
+
+gulp.task('default',  gulp.series(clean, copyIndex));
+
+function clean(done) {
+    del(['dist']);
+    done();  //# Async callback for completion.
+}
+
+function copyIndex(done) {
+    return gulp.src('./src/index.html').pipe(gulp.dest('./dist', {overwrite: true}));
+}
